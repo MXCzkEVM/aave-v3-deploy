4f9d028223d41e93f751ce40a4693658d7f7d93f
Sheen <hx.sheen@gmail.com>
2023-05-16 20:28:39 +0200

    Update index.ts

--- markets/ethereum/index.ts
+++ markets/ethereum/index.ts
@@ -8,12 +8,7 @@ import { AaveMarket } from "../aave/index";
 export const EthereumV3Market: IAaveConfiguration = {
   ...AaveMarket,
   ProviderId: 30,
-  WrappedNativeTokenSymbol: "WETH",
-  MarketId: "Aave Ethereum Market",
-  ATokenNamePrefix: "Ethereum",
-  StableDebtTokenNamePrefix: "Ethereum",
-  VariableDebtTokenNamePrefix: "Ethereum",
-  SymbolPrefix: "Eth",
+  WrappedNativeTokenSymbol: "WMXC",
   ReserveAssets: {},
   ChainlinkAggregator: {},
   ReservesConfig: {},
